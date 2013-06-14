f0accd6ed5448ac08b8429c1a539c50852a90598
Mark Hayes <mark@zurb.com>
2013-06-14 10:24:31 -0700

    require sass_variable_extractor in create variables file script

--- create_variable_sass_file.rb
+++ create_variable_sass_file.rb
@@ -1,48 +1,6 @@
 #!/usr/bin/env ruby
 # USAGE ./create_variable_sass_file.rb
-require "set"
-
-class SassVariableExtractor
-  # Format: $body-bg: blue !default; // Line comment
-  # Match:      1       2            3  4
-  REGEX = /\$([^:]+):\s*([^; ]+) !default;\s*(\/\/\s*(.*))?/
-  def initialize(filepath)
-    @file = File.open(filepath, "r")
-    @vars = Set.new
-  end
-
-  def extract_sass_variables
-    @file.rewind
-    @file.lines.each do |line|
-      
-      if match=REGEX.match(line)
-        @vars.add({
-          name: match[1],
-          value: match[2],
-          comment: match[4]
-        })
-      end
-    end
-    @vars
-  end
-
-  def extract_docs
-    @file.rewind
-    docs = "\n// #{File.basename(@file.path)}\n"
-    started = false
-    @file.lines.each do |line|
-      if started
-        docs += line
-        return docs if line.start_with?("// *")
-      end
-      if line.start_with?("// *") && !started
-        started = true
-        docs += line
-      end
-    end
-    return docs
-  end
-end
+require "./lib/foundation/utils/sass_variable_extractor"
 
 output_file = File.new("./scss/foundation/_variables.scss", "w")
 sass_files = Dir.glob("./scss/foundation/components/**/_*.scss")
